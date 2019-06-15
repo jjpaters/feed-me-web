00e3d629dec4b4df8114745418d31b2de127f213
Justin Paterson <jpaterson06@gmail.com>
2019-06-14 21:35:17 -0400

    Fix prod redirect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     clientId: 'wE9ro7uXyS2JTn0mcXItFW6ifLdCHNSP',
     clientDomain: 'jjpaters.auth0.com',
     audience: 'https://feed-me-already-api.herokuapp.com/api/',
-    redirect: 'http://localhost:4200/callback',
+    redirect: 'https://feed-me-already.herokuapp.com/callback',
     scope: 'openid profile'
   },
   self: 'https://feed-me-already.herokuapp.com/'
