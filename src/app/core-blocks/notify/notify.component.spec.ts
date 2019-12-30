c0751f5baa17127969802ec356801e85411b28a0
jpaterson <JustinPaterson@quickenloans.com>
2019-12-30 11:20:43 -0500

    Add notify component tests

--- src/app/core-blocks/notify/notify.component.spec.ts
+++ src/app/core-blocks/notify/notify.component.spec.ts
@@ -1,11 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { NotifyComponent } from './notify.component';
 
 describe('NotifyComponent', () => {
   let component: NotifyComponent;
   let fixture: ComponentFixture<NotifyComponent>;
+  let activeModal: NgbActiveModal;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,10 +15,8 @@ describe('NotifyComponent', () => {
         NgbModule
       ],
       providers: [
-        {
-          provide: NgbActiveModal,
-          useValue: {}
-        }
+        NgbModal,
+        NgbActiveModal
       ]
     })
     .compileComponents();
@@ -27,10 +26,17 @@ describe('NotifyComponent', () => {
     fixture = TestBed.createComponent(NotifyComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    activeModal = TestBed.get(NgbActiveModal);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dismiss when close() is called', () => {
+    spyOn(activeModal, 'dismiss');
+    component.close();
+    expect(activeModal.dismiss).toHaveBeenCalled();
   });
 
 });
