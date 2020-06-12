bcda9832b6fdbb82bd79937cde16c630ac059d1f
Justin Paterson <jpaterson06@gmail.com>
2020-06-12 11:25:14 -0400

    Add unit tests

--- src/app/core-blocks/notify/notify.component.spec.ts
+++ src/app/core-blocks/notify/notify.component.spec.ts
@@ -2,10 +2,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { NotifyComponent } from './notify.component';
+import { NotifyService } from './notify.service';
 
 describe('NotifyComponent', () => {
   let component: NotifyComponent;
   let fixture: ComponentFixture<NotifyComponent>;
+  let notifyService: NotifyService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,11 +23,18 @@ describe('NotifyComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NotifyComponent);
     component = fixture.componentInstance;
+    notifyService = TestBed.get(NotifyService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('close should clear the notification', () => {
+    const spy = spyOn(notifyService, 'clear');
+    component.close();
+    expect(spy).toHaveBeenCalled();
   });
 
 });
