45ce45c5ff45a01f2a8cf6d55ad8d17c246892be
Justin Paterson <jpaterson06@gmail.com>
2021-08-18 09:42:48 -0400

    Fix nav

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -59,7 +59,7 @@ import '@angular/localize/init';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 /***************************************************************************************************
  * Currently, the newest versions of Angular (6+) do not include shims for 'global' or 'process' which were provided in previous versions.
