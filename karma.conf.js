d75d54af2cc97c9a5ad26bbdd82444820827ad0b
Justin Paterson <jpaterson06@gmail.com>
2021-08-19 13:24:56 -0400

    wildcard

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
