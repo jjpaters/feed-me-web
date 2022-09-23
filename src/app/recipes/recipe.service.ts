c6fb96d83800c1b9f2685c7ea9837e6533df2c16
Justin Paterson <17819020+jjpaters@users.noreply.github.com>
2022-09-23 08:19:06 -0400

    Fix dependabot

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,30 +12,25 @@ export class RecipeService {
   constructor(private http: HttpClient) { }
 
   async createRecipe(recipe: Recipe): Promise<Recipe> {
-    const userId = `1`;
     const res = await this.http.post<Recipe>(`${environment.api}/recipes`, recipe).toPromise();
     return res;
   }
 
   async deleteRecipe(recipeId: string): Promise<void> {
-    const userId = `1`;
     await this.http.delete(`${environment.api}/recipes/${recipeId}`).toPromise();
   }
 
   async getRecipes(): Promise<Recipe[]> {
-    const userId = `1`;
     const res = await this.http.get<Recipe[]>(`${environment.api}/recipes`).toPromise();
     return res;
   }
 
   async getRecipe(recipeId: string): Promise<Recipe> {
-    const userId = `1`;
     const res = await this.http.get<Recipe>(`${environment.api}/recipes/${recipeId}`).toPromise();
     return res;
   }
 
   async updateRecipe(recipe: Recipe): Promise<Recipe> {
-    const userId = `1`;
     const res = await this.http.patch<Recipe>(`${environment.api}/recipes/${recipe.recipeId}`, recipe).toPromise();
     return res;
   }
