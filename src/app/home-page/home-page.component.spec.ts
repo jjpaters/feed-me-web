7d3f2a821fb85ae25bc0c9d2ec6b6346daab73e2
jjpaters <jpaterson06@gmail.com>
2023-09-08 15:17:22 -0400

    add unit tests

--- src/app/home-page/home-page.component.spec.ts
+++ src/app/home-page/home-page.component.spec.ts
@@ -1,8 +1,9 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CoreBlocksModule } from '../core-blocks/core-blocks.module';
 
 import { HomePageComponent } from './home-page.component';
+import { AuthService } from '@auth0/auth0-angular';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
@@ -28,4 +29,11 @@ describe('HomePageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should invoke authService.loginWithPopup', inject([AuthService], (authService: AuthService) => {
+    spyOn(authService, 'loginWithPopup'); 
+    component.logIn();
+    expect(authService.loginWithPopup).toHaveBeenCalled();
+  }));
+
 });
