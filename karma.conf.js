1624f2e2499370e00d78f6db2181fc0ea047b61c
jjpaters <jpaterson06@gmail.com>
2023-09-08 10:48:54 -0400

    junit path

--- karma.conf.js
+++ karma.conf.js
@@ -26,8 +26,8 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     junitReporter: {
-      outputDir: 'coverage/feed-me-web/', // results will be saved as $outputDir/$browserName.xml
-      outputFile: 'coverage.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      outputDir: 'coverage/feed-me-web/junit', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'test-results.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: '', // suite will become the package name attribute in xml testsuite element
       useBrowserName: false, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
